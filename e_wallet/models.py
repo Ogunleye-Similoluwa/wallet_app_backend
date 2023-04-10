d059b46f3d25199c92ac779c535658ce1e05d2c1
Simex <osimiloluwa9@example.com>
2023-04-10 22:44:39 +0100

    models done

--- e_wallet/models.py
+++ e_wallet/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 
-#
-
 class User(models.Model):
     user_id = models.IntegerField(primary_key=True, auto_created=True)
     name = models.CharField(max_length=40, default="Full Name", null=False, blank=False)
@@ -11,6 +9,8 @@ class User(models.Model):
     password = models.CharField(max_length=20, default="")
     phone_number = models.CharField(max_length=12, null=False, blank=False)
     date = models.DateTimeField(auto_now=True, null=False, blank=False)
+    wallet = models.ForeignKey("Wallet", on_delete=models.CASCADE)
+    notification = models.OneToOneField("Notification", on_delete=models.CASCADE)
 
 
 class Transaction(models.Model):
@@ -25,6 +25,8 @@ class Transaction(models.Model):
     transaction_status = models.CharField(blank=False, null=False, choices=TRANSACTION_STATUS)
     amount = models.DecimalField(blank=False, null=False)
     date = models.DateTimeField(auto_now=True, null=False, blank=False)
+    wallet = models.ManyToOneRel("Wallet", on_delete=models.CASCADE, related_name="wallet")
+    notification = models.OneToOneField("Notification", on_delete=models.CASCADE)
 
 
 class CreditCard(models.Model):
@@ -41,11 +43,21 @@ class Account(models.Model):
     bank_number = models.CharField()
 
 
-class Beneficiary(models.Model):
-    beneficiary_id = models.IntegerField(auto_created=True)
-
-
 class Wallet(models.Model):
     wallet_id = models.IntegerField(primary_key=True, auto_created=True)
     balance = models.DecimalField(default=0, max_digits=7, decimal_places=2)
-    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+
+class Notification(models.Model):
+    notification_id = models.IntegerField(primary_key=True, auto_created=True)
+    message = models.CharField(max_length=200, null=True, blank=True)
+    time_stamp = models.DateTimeField(auto_now=True)
+
+
+class Admin(models.Model):
+    admin_id = models.IntegerField(primary_key=True, auto_created=True)
+    name = models.CharField(null=False, blank=False, max_length=200)
+    username = models.CharField(max_length=20, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    password = models.CharField(max_length=20, default="")
